2ccf3fcba52ed15eec7aa17aa3d779fb8a422131
nobii <fujisawa-shin@kayac.com>
2017-12-04 20:11:23 +0900

    調整

--- src/js/script.js
+++ src/js/script.js
@@ -1,38 +1,49 @@
 import { Howl } from 'howler';
 
+const BPM = 158; // beat per minutes = 1分に何ビート打つか
+const BEATS = 36 * 4; // ビート数 = 小節数 × 4拍子
+const DURATION = 60 * BEATS / BPM;
+
 const dom = {
-    button: document.querySelector('#button'),
+    toggle: document.querySelector('#toggle'),
+    skip: document.querySelector('#skip'),
     time: document.querySelector('#time')
 };
 
 const sound = new Howl({
     src: [ '/8bit18.mp3' ],
-    loop: true
+    loop: false // 手動loopなので不要
 });
 
 let flag = false;
 
 function loop () {
     const seconds = sound.seek();
-    const duration = sound.duration();
-    if (!isNaN(seconds)) {
-        dom.time.innerHTML = `${Math.floor(seconds)}s / ${Math.floor(duration)}s`;
+    if (!isNaN(seconds) && !isNaN(DURATION)) {
+        dom.time.innerHTML = `${Math.floor(seconds)}s / ${Math.floor(DURATION)}s`;
+    }
+    if (seconds > DURATION) {
+        sound.seek(seconds - DURATION);
     }
     requestAnimationFrame(loop);
 }
 
 sound.on('play', () => {
     flag = true;
-    dom.button.innerHTML = 'pause';
+    dom.toggle.innerHTML = 'pause';
 });
 
 sound.on('pause', () => {
     flag = false;
-    dom.button.innerHTML = 'resume';
+    dom.toggle.innerHTML = 'resume';
 });
 
-dom.button.addEventListener('click', () => {
+dom.toggle.addEventListener('click', () => {
     flag ? sound.pause() : sound.play();
+});
+
+dom.skip.addEventListener('click', () => {
+    sound.seek(50);
 });
 
 loop();
